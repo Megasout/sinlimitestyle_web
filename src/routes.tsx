b9ff56bee71b8a49f47d99517c2333d17e144e24
Megasout <Megasoutt@gmail.com>
2024-10-03 13:07:41 -0300

    imports and logo

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./Pages/Home";
+import Layout from "./Components/Layout";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element:<Home/>
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }
+        ]
     }
 ])
 
