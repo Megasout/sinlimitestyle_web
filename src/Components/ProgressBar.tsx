99734246730f2d245a6d32703e229c7cdf1dde71
Megasout <Megasoutt@gmail.com>
2024-10-05 17:43:00 -0300

    Progress Bar

---
+++ src/Components/ProgressBar.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useRef, useState } from "react"
+
+type ProgressBarType = {
+    length: number,
+    select: number,
+    pause: boolean,
+    setSelect: (value: number) => void
+}
+
+function ProgressBar(props: ProgressBarType) {
+    const { length, select, setSelect, pause } = props
+    const [change, setChange] = useState(false)
+    const intervalRef = useRef<number | undefined>();
+
+    const handleOnClick = (index: number) => {
+        setSelect(index)
+    }
+
+    useEffect(() => {
+        clearInterval(intervalRef.current)
+        setChange(false)
+        intervalRef.current = setTimeout(() => {
+            setChange(true)
+        }, 3000);
+    }, [select])
+
+    useEffect(() => {
+        if (pause) return
+
+        if (change) {
+            next(length, setSelect, select + 1)
+            setChange(false)
+        }
+    }, [pause, change])
+
+    return (
+        <div className="progress_bar">
+            {Array.from({ length: length }).map((_, index) =>
+                <div
+                    key={index}
+                    onClick={() => handleOnClick(index)}
+                    className={`bar ${select == index ? 'select' : ''}`}></div>
+            )}
+        </div>
+    )
+}
+
+export default ProgressBar
+
+function next(length: number, setSelect: (value: number) => void, value: number) {
+    if (value >= length)
+        return setSelect(0)
+
+    setSelect(value)
+}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
+import "../Styles/home.scss"
+import ProgressBar from "../Components/ProgressBar"
+
 function Home() {
+    const [select, setSelect] = useState(4)
+    const [pause, setPause] = useState<boolean>(false)
+
     return (
-        <div>
-            <div className="banner"></div>
+        <div className="home">
+            <div className="banner">
+
+                <ProgressBar
+                    length={6}
+                    select={select}
+                    setSelect={setSelect}
+                    pause={pause} />
+            </div>
             <center>
                 {/* <h1>Home</h1> */}
             </center>
