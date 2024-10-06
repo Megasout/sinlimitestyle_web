ee371d3752744530721691311e33acac8912de7e
Megasout <Megasoutt@gmail.com>
2024-10-06 13:14:26 -0300

    Recommendations design

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,15 +1,16 @@
 import "../Styles/home.scss"
 import Banner from "../Components/Home/Banner"
 import Recommendations from "../Components/Home/Recommendations"
+import useWindowWidth from "../Hooks/useWindowWidth"
 
 function Home() {
-
+    const width = useWindowWidth()
 
 
     return (
         <div className="home">
             <Banner />
-            <Recommendations />
+            <Recommendations width={width}/>
         </div>
     )
 }
