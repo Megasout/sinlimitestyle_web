8d6351211ebe1b2cbf2e1810c7bb43ca065f707f
Megasout <Megasoutt@gmail.com>
2024-10-17 21:56:13 -0300

    options

--- src/Pages/Shop.tsx
+++ src/Pages/Shop.tsx
@@ -5,7 +5,41 @@ function Shop() {
     return (
         <div className="shop">
             <Filters />
-            <div className="content"></div>
+            <div className="content">
+                <div className="options">
+                    <div className="button">
+                        <span
+                            translate="no"
+                            className="material-symbols-outlined">
+                            grid_view
+                        </span>
+                    </div>
+                    <div className="button">
+                        <span
+                            translate="no"
+                            className="material-symbols-outlined">
+                            list
+                        </span>
+                    </div>
+                    <div className="sorting">
+                        <label>Ordenar por </label>
+                        <select>
+                            <option>Relevancia</option>
+                            <option>Mayor Precio</option>
+                            <option>Menor Precio</option>
+                        </select>
+                    </div>
+                    <div style={{width: '100%'}}></div>
+                    <div className="search">
+                        <input type="text" placeholder="Buscar Productos..."></input>
+                        <span
+                            translate="no"
+                            className="material-symbols-outlined">
+                            search
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
